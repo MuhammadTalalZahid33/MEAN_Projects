29e8a2b3de41cda75901f3cefa17ab9717d33238
Talal <talalzahid912@gmail.com>
2025-11-26 15:33:55 +0500

    installed some important packages(dotenv, express, mongoose_
    established db connection and write our note schema -> model....

--- NotesMaker/Backend/src/index.js
+++ NotesMaker/Backend/src/index.js
@@ -1,3 +1,20 @@
 import dotenv from "dotenv"
+import connectDB from "./config/db.js" 
+import { app } from "./app.js"
 
 dotenv.config({path: './.env'})
+const PORT = process.env.PORT
+
+connectDB()
+.then(() => {
+    app.on("error", (error) => {
+        console.log("db connection established but express unable to talk", error);
+        throw(error)
+    })
+    app.listen(PORT || 5000, () => {
+        console.log(`Server is running at port http://localhost:${PORT}`);
+    }) 
+})
+.catch((error) => {
+    console.log("db connection error: ", error);
+})
