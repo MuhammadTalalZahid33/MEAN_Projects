a88f2db53ca319c3abca776e1ef243dd484a2253
Talal <talalzahid912@gmail.com>
2025-12-08 20:02:32 +0500

    ...

--- NotesMaker/Frontend/src/app/auth/register/register.component.ts
+++ NotesMaker/Frontend/src/app/auth/register/register.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  registorForm !: FormGroup;
 
+  firstName = new FormControl('', [Validators.required ,Validators.minLength(4)]);
+  lastName = new FormControl('');
+  userName = new FormControl('', [Validators.required, Validators.minLength(6)]);
+  password = new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]);
+
+  onSubmit(){
+
+  }
 }
