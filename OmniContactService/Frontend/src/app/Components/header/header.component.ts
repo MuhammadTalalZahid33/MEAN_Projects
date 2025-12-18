1c1ea296956134bcd8a720e9ee1b869f191de0c0
Talal <talalzahid912@gmail.com>
2025-12-18 20:32:54 +0500

    making the header for my application...

--- OmniContactService/Frontend/src/app/Components/header/header.component.ts
+++ OmniContactService/Frontend/src/app/Components/header/header.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import { MatRipple } from "@angular/material/core";
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, MatMenuModule, MatButtonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
--- OmniContactService/Frontend/src/app/app.routes.ts
+++ OmniContactService/Frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './Components/header/header.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'header',
+        component: HeaderComponent,
+        // loadChildren: 
+    }
+];
