fc849303d145e448e9446809e4c7db16c7095559
Talal <talalzahid912@gmail.com>
2025-12-19 16:17:21 +0500

    header done ...

--- OmniContactService/Frontend/src/app/app.config.ts
+++ OmniContactService/Frontend/src/app/app.config.ts
@@ -1,13 +1,20 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MAT_MENU_DEFAULT_OPTIONS } from '@angular/material/menu';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideAnimationsAsync()
+    provideAnimationsAsync(),
+    {
+      provide: MAT_MENU_DEFAULT_OPTIONS,
+      useValue: {
+        overlayPanelClass: 'my-menu-panel'
+      }
+    }
   ],
 };
