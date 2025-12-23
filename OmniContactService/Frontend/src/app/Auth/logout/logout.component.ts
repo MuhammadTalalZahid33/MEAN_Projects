999f5a73bf41a3f66e6c99f05dbc356fb19b0ff7
Talal <talalzahid912@gmail.com>
2025-12-23 18:32:52 +0500

    implementing logout functionality

---
+++ OmniContactService/Frontend/src/app/Auth/logout/logout.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import "amazon-connect-streams";
+
+@Component({
+  selector: 'app-logout',
+  standalone: true,
+  imports: [],
+  templateUrl: './logout.component.html',
+  styleUrl: './logout.component.scss'
+})
+export class LogoutComponent {
+    
+}
