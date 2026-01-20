88eebf304dc733c569d2938d031868bfe51ede3e
Talal <talalzahid912@gmail.com>
2026-01-20 13:26:09 +0500

    auth service for login/logout... added basic routes added api routes in constants file

--- ProjectManagementSystem/Frontend/src/app/app.routes.ts
+++ ProjectManagementSystem/Frontend/src/app/app.routes.ts
@@ -1,3 +1,40 @@
 import { Routes } from '@angular/router';
+import { loginGuard } from './core/guards/login.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./auth/login/login.component')
+            .then(m => m.LoginComponent),
+        canActivate: [loginGuard]
+    },
+    {
+        path: 'main',
+        loadComponent: () => {
+            return import('./layout/main-layout/main-layout.component')
+                .then(m => m.MainLayoutComponent)
+        },
+        children: [
+            {
+                path: 'dashboard',
+                loadComponent: () => {
+                    return import('./pages/dashboard/dashboard.component')
+                        .then(m => m.DashboardComponent)
+                }
+            },
+            {
+                path: 'settings',
+                loadComponent: () => {
+                    return import('./pages/settings/settings.component')
+                        .then(m => m.SettingsComponent)
+                }
+            },
+            {
+                path: '',
+                redirectTo: 'dashboard',
+                pathMatch: 'full'
+            }
+
+        ]
+    }
+];
---
+++ ProjectManagementSystem/Frontend/src/app/core/constants.ts
@@ -0,0 +1,14 @@
+//Public Route
+const authApi = "https://localhost:5000/api/v1/auth";
+
+//Protected Route
+const userApi = "https://localhost:5000/api/v1/users";
+const projectApi = "https://localhost:5000/api/v1/projects";
+const taskApi = "https://localhost:5000/api/v1/tasks";
+
+export {
+    authApi,
+    userApi,
+    projectApi,
+    taskApi
+};
