c7d6564d43b17c1f43c43bf0b1c9e349a8fb8d53
Talal <talalzahid912@gmail.com>
2026-02-05 18:48:08 +0500

    sidebar autoclose feature added...

--- ProjectManagementSystem/Frontend/src/app/components/sidebar/sidebar.component.ts
+++ ProjectManagementSystem/Frontend/src/app/components/sidebar/sidebar.component.ts
@@ -1,10 +1,11 @@
-import { Component, inject, signal, Signal } from '@angular/core';
+import { Component, inject, signal, Signal, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
 import { MatIcon } from "@angular/material/icon";
 import { Router, RouterLink, RouterOutlet, RouterLinkActive } from "@angular/router";
 import { AuthService } from '../../services/auth.service';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-sidebar',
@@ -14,7 +15,18 @@ import { AuthService } from '../../services/auth.service';
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
-  constructor() { }
   private authService = inject(AuthService);
   role = this.authService.userRole
+  isMobile = signal(false);
+
+  //Sidebar Auto Close
+  private breakpointObserver = inject(BreakpointObserver);
+
+   constructor() {
+    this.breakpointObserver
+      .observe(['(max-width: 900px)'])
+      .subscribe(result => {
+        this.isMobile.set(result.matches);
+      });
+  }
 }
